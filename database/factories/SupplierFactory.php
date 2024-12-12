44b9558d8cd12b1f45ec7616743c6888207957d3
Stevanus Cahya Anggara <stevanuschyaanggara@gmail.com>
2024-12-12 09:47:24 +0700

    Feat : Adding New Table

---
+++ database/factories/SupplierFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SupplierFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = \App\Models\Supplier::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->company,
+            'email' => $this->faker->unique()->safeEmail,
+            'phone' => $this->faker->phoneNumber,
+            'address' => $this->faker->address,
+            'contact_person' => $this->faker->name,
+            'status' => $this->faker->randomElement(['active', 'inactive']),
+        ];
+    }
+}
