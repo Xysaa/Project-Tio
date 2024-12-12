5b18238664d8c2f6fa3a15a8d64632f308dea36c
Stevanus Cahya Anggara <stevanuschyaanggara@gmail.com>
2024-12-12 09:48:51 +0700

    Update : Fixing Routes

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\SupplierController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::get('/', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-Route::get('/dashboard',function(){
-    return view('dashboard');
-});
-
-
 Route::get('/dashboard', [AuthController::class, 'dashboard'])
     ->name('dashboard')
     ->middleware('auth');
-
 Route::resource('items', ItemController::class);
 Route::resource('transactions', TransactionController::class);
+Route::resource('suppliers', SupplierController::class)
+    ->middleware('auth');
